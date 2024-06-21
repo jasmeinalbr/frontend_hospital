a7014a5a135163274a7607cfbfe682dbcf0fb96b
jasmeinalbr <jasmeinalbaar29@gmail.com>
2024-06-21 20:38:44 +0700

    tolong

---
+++ src/app/appointment/appointment.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { AppointmentService } from '../appointment.service';
+import { Appointment } from '../appointment';
+
+@Component({
+  selector: 'app-appointment',
+  templateUrl: './appointment.component.html',
+  styleUrl: './appointment.component.css'
+})
+export class AppointmentComponent {
+  
+  appointments:Appointment[]=[]
+  constructor(private appointmentService:AppointmentService) { }
+
+  ngOnInit():void{
+    this.getAppointments();
+  }
+
+  getAppointments(){
+    this.appointmentService.getAllAppointment().subscribe(data=>{
+      this.appointments=data;
+    })
+  }
+}
